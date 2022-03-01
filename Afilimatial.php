fe923d7716b5bb3633808c84c4177287e08766a1
Ampeire <ampeireedgar5@gmail.com>
2022-03-01 13:20:33 +0300

    Initial Commit

---
+++ Afilimatial.php
@@ -0,0 +1,34 @@
+<?php
+
+
+/**
+ * @package Afilimatial
+ * @author Ampeire Edgar
+ * @license GPL V2 or later
+ */
+
+/*
+ * Plugin Name: Afilimatial
+ * Plugin URI: https://github.com/dancan-edgar/afilimatial.git
+ * Description: Afilimatial enables you to cature the current category from the current page.
+ * Version: 1.0.0
+ * Author: Ampeire Edgar
+ * Author URI: https://github.com/dancan-edgar
+ * License: GPL V2 or higher
+ * Text Domain: afilimatial
+ */
+
+/*
+Afilimatial is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Afilimatial is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Afilimatial. If not, see https://www.gnu.org/licenses/old-licenses/gpl-2.0.html}.
+*/
